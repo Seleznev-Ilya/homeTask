15999e5781ccd66ef490701b14cb9c8ff4b3b96c
Selieznov Illia <ziziron21@gmail.com>
2021-07-21 10:09:12 +0300

    v6

--- script.js
+++ script.js
@@ -1,57 +1,25 @@
 // Imagine having information about recent supermarket visit of `customers` find out who spent the most
 // You are not allowed using: loops, if, else, switch, var or let statements
 
-const concatPurchaseList = (list) => list.reduce((accumulator, currentItem) => accumulator.concat(currentItem))
+const concatLists = list => list.reduce((accumulator, currentItem) => accumulator.concat(currentItem))
 
-const getPurchaseList = (receiptsList, id) => {
-  const purchaseList = receiptsList
-    .filter(customer => customer.customerId === id)
-    .map(item => item.list)
-  return concatPurchaseList(purchaseList)
-}
+const concatCostsList = list => concatLists(list.map(item => item.list))
 
-const convertToValues = (list) => list.map(purchase => {
-  const { price } = goods.find(good => good.id == purchase.goodId)
-  return Number((price * purchase.amount).toFixed(2))
-})
+const getCostsList = lists => id => concatCostsList(lists.receipts.filter(item => item.customerId === id))
 
-const getCostSum = (list) => list.reduce((sum, current) => sum + current, 0)
+const reducePriceList = (list) => list.reduce((sum, current) => sum + current, 0)
 
-const createCustomer = (list, id) => {
-  const convertedPriceList = convertToValues(getPurchaseList(list, id))
-  const cost = getCostSum(convertedPriceList)
-  const { name } = customers.find(name => name.id === id)
-  return {
-    id,
-    name,
-    cost,
-  }
-}
+const getPriceList = goods => list =>  list.map( item => Number((goods.find(good => good.id === item.goodId).price * item.amount).toFixed(2)))
 
-const removeDuplicates = (list, key = item => item.id) => [
-  ...new Map(list
-    .map(item => [key(item), item]))
-    .values()
-]
+const getSumCosts = lists => costsList => reducePriceList(getPriceList(lists.goods)(costsList))
 
-const sortBySpentTheMost = list => list.sort((a, b) => b.cost - a.cost)
-
-const convertToFormatList = list => sortBySpentTheMost(removeDuplicates(list))
-
-const getCustomersList = list => {
-  const receiptsList = list.map(purchase => createCustomer(list, purchase.customerId))
-  return convertToFormatList(receiptsList)
-}
+const getCustomersCostsList = (customers, sumFn, listFn) => customers.map(item => ({ ...item, cost: sumFn(listFn(item.id)) }))
 
 const getCustomerSpentTheMost = list => list?.length ? list[0] : null
 
-const response = (receiptsList = []) => {
-  const customers = getCustomersList(receiptsList)
-  const customerSpentTheMost = getCustomerSpentTheMost(customers)
-  return {
-    customers,
-    customerSpentTheMost,
-  }
-}
+const customersCostsList = lists => getCustomersCostsList(lists.customers, getSumCosts(lists), getCostsList(lists))
 
-console.log(response(receipts));
+const response = lists => ({ spentTheMost: getCustomerSpentTheMost(customersCostsList(lists)), customers: customersCostsList(lists)})
+
+console.log(response({ customers, receipts, goods }));
+
