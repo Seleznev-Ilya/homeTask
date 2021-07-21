abaa704f86859b0b13bf2cbbe9748d656b648149
Selieznov Illia <ziziron21@gmail.com>
2021-07-21 16:05:47 +0300

    v6.1

--- script.js
+++ script.js
@@ -3,11 +3,11 @@
 
 const concatLists = list => list.reduce((accumulator, currentItem) => accumulator.concat(currentItem))
 
-const concatCostsList = list => concatLists(list.map(item => item.list))
+const concatCostsLists = list => concatLists(list.map(item => item.list))
 
-const getCostsList = lists => id => concatCostsList(lists.receipts.filter(item => item.customerId === id))
+const getCostsList = lists => id => concatCostsLists(lists.receipts.filter(item => item.customerId === id))
 
-const reducePriceList = (list) => list.reduce((sum, current) => sum + current, 0)
+const reducePriceList = list => list.reduce((sum, current) => sum + current, 0)
 
 const getPriceList = goods => list =>  list.map( item => Number((goods.find(good => good.id === item.goodId).price * item.amount).toFixed(2)))
 
@@ -15,11 +15,11 @@ const getSumCosts = lists => costsList => reducePriceList(getPriceList(lists.goo
 
 const getCustomersCostsList = (customers, sumFn, listFn) => customers.map(item => ({ ...item, cost: sumFn(listFn(item.id)) }))
 
-const getCustomerSpentTheMost = list => list?.length ? list[0] : null
+const getSpentTheMost = list => list?.length ? list[0] : null
 
 const customersCostsList = lists => getCustomersCostsList(lists.customers, getSumCosts(lists), getCostsList(lists))
 
-const response = lists => ({ spentTheMost: getCustomerSpentTheMost(customersCostsList(lists)), customers: customersCostsList(lists)})
+const response = lists => ({ spentTheMost: getSpentTheMost(customersCostsList(lists)), customers: customersCostsList(lists)})
 
 console.log(response({ customers, receipts, goods }));
 
